170deba9f3b00f503a78e98b7bdef336afede954
Gevorg Davoian <davoian.serf@gmail.com>
2017-02-27 14:38:37 +0200

    Collect and serve static files for admin panel

--- sentence_split/dev_settings.py
+++ sentence_split/dev_settings.py
@@ -1,6 +1,11 @@
 from .settings import *
 
 
+INSTALLED_APPS += [
+    'storages',
+]
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -11,3 +16,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAJJYNXM63VQWA7R5Q'
+
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'zappa-a5q9128wh'
